9193ed18b7ea97a1046983fa32cf29556912a773
IustinDragan <iustindragan@gmail.com>
2021-08-29 09:11:27 +0300

    Update curs 5(inainte de a crea dogAdapter etc)

--- src/main/java/com/p5/adoptions/controllers/AnimalShelterController.java
+++ src/main/java/com/p5/adoptions/controllers/AnimalShelterController.java
@@ -2,13 +2,24 @@ package com.p5.adoptions.controllers;
 
 
 import com.p5.adoptions.repository.cats.Cat;
+import com.p5.adoptions.repository.dogs.Dog;
 import com.p5.adoptions.repository.shelter.AnimalShelter;
 import com.p5.adoptions.service.AnimalShelterService;
+import com.p5.adoptions.service.DTO.ListDTO;
+import com.p5.adoptions.service.DTO.ShelterDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+//Controller-ul depinde doar de Service.
+//Service-ul este dependent de Repository.
+//Servici-ul nu depinde de Web
+//Repozitory nu depinde de Serviciu
+//DTO = Data Tansport Object
+
 
 @RestController
 @RequestMapping("/api/v1/shelters")
@@ -21,21 +32,22 @@ public class AnimalShelterController {
     }
 
     @GetMapping() //citire de lista
-    public ResponseEntity <List<AnimalShelter>> getShelters(){
+    public ResponseEntity <ListDTO<ShelterDTO>> getShelters(){
         return ResponseEntity.ok(animalShelterService.findAll());
     }
     @GetMapping("/{id}")  //citire de id
-    public ResponseEntity <AnimalShelter> getShelter(@PathVariable("id") Integer id){
+    public ResponseEntity <ShelterDTO> getShelter(@PathVariable("id") Integer id){
         return ResponseEntity.ok(animalShelterService.findById(id));
     }
 
     @PostMapping() //creare
-    public ResponseEntity<AnimalShelter> createShelter(@RequestBody AnimalShelter animalShelter){
+    public ResponseEntity<ShelterDTO> createShelter(@Valid @RequestBody ShelterDTO animalShelter){
         return ResponseEntity.ok(animalShelterService.createShelter(animalShelter));
     }
 
     @PutMapping("/{id}") //modificare
-    public ResponseEntity<AnimalShelter> updateShelter(@PathVariable("id") Integer id, @RequestBody AnimalShelter animalShelter){
+    public ResponseEntity<ShelterDTO> updateShelter(@PathVariable("id") Integer id,
+                                                     @Valid @RequestBody ShelterDTO animalShelter){
         return ResponseEntity.ok(animalShelterService.updateShelter(id, animalShelter));
     }
     @DeleteMapping("/{id}") //stergere
@@ -54,4 +66,37 @@ public class AnimalShelterController {
     public ResponseEntity<List<Cat>> addNewCatToShelter(@PathVariable("shelterId") Integer shelterId, @RequestBody Cat cat){
         return ResponseEntity.ok(animalShelterService.addNewCatToShelter(shelterId, cat));
     }
+    //adaugat de pe gitHub
+
+    @PatchMapping("/{shelterId}/cats/{catId}")
+    public ResponseEntity<Cat> updateCatInShelter(@PathVariable("shelterId") Integer shelterId, @PathVariable("catId") Integer catId,
+                                                  @RequestBody Cat cat){
+        return ResponseEntity.ok(animalShelterService.updateCatInShelter(shelterId, catId, cat));
+    }
+    @DeleteMapping("/{shelterId}/cats/{catId}")
+    public ResponseEntity<Object> deleteCatInShelter(@PathVariable("shelterId") Integer shelterId, @PathVariable("catID") Integer catId){
+        animalShelterService.deleteCatInShelter(shelterId, catId);
+        return ResponseEntity.status(HttpStatus.GONE).build();
+    }
+    @GetMapping("/{shelterId}/dogs")
+    public ResponseEntity<List<Dog>> getDogsForShelter(@PathVariable("shelterId") Integer shelterId){
+        return ResponseEntity.ok(animalShelterService.findAllDogsByShelter(shelterId));
+    }
+    @PutMapping("/{shelterId}/dogs")
+    public ResponseEntity<List<Dog>> addNewDogToShelter(@PathVariable("shelterId") Integer shelterId, @RequestBody Dog dog){
+        return ResponseEntity.ok(animalShelterService.addNewDogToShelter(shelterId, dog));
+    }
+    @PutMapping("/{shelterId}/dogs/{dogId}")
+    public ResponseEntity<Dog> undateDogInShelter(@PathVariable("shelterId") Integer shelterId, @PathVariable("dogId") Integer dogId,
+                                                  @RequestBody Dog dog){
+        return ResponseEntity.ok(animalShelterService.updateDogInShelter(shelterId,dogId,dog));
+    }
+    @DeleteMapping("/{shelterId}/dogs/{dogId}")
+    public ResponseEntity<Object> deleteDogInShelter(@PathVariable("shelterId") Integer shelterId, @PathVariable("dogId") Integer dogId){
+        animalShelterService.deleteDogInShelter(shelterId, dogId);
+        return ResponseEntity.status(HttpStatus.GONE).build();
+    }
+
+
+
 }
--- src/main/java/com/p5/adoptions/service/AnimalShelterService.java
+++ src/main/java/com/p5/adoptions/service/AnimalShelterService.java
@@ -1,16 +1,32 @@
 package com.p5.adoptions.service;
 
 import com.p5.adoptions.repository.cats.Cat;
+import com.p5.adoptions.repository.dogs.Dog;
 import com.p5.adoptions.repository.shelter.AnimalShelter;
 import com.p5.adoptions.repository.shelter.AnimalShelterRepository;
+import com.p5.adoptions.service.DTO.ListDTO;
+import com.p5.adoptions.service.DTO.ShelterDTO;
+import com.p5.adoptions.service.adapters.ShelterAdapter;
+import com.p5.adoptions.service.validations.OnCreate;
+import com.p5.adoptions.service.validations.OnUpdate;
 import org.hibernate.cache.spi.CacheTransactionSynchronization;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 @Service
+@Validated
 public class AnimalShelterService
 {
     private final AnimalShelterRepository animalShelterRepository;
@@ -20,28 +36,46 @@ public class AnimalShelterService
         this.animalShelterRepository = animalShelterRepository;
     }
 
-    public List<AnimalShelter > findAll() {
-        return animalShelterRepository.findAll();
+    public ListDTO<ShelterDTO> findAll() {
+        List<ShelterDTO> data =
+                ShelterAdapter.toDTOList(animalShelterRepository.findAll());
+
+        Long totalCount = animalShelterRepository.count();
+
+        ListDTO<ShelterDTO> response = new ListDTO<>();
+        response.setData(data);
+        response.setTotalCount(totalCount);
+
+        return response;
     }
 
-    public AnimalShelter createShelter(AnimalShelter animalShelter) {
-        return animalShelterRepository.save(animalShelter);
+    @Validated(OnCreate.class)
+    public ShelterDTO createShelter(@Valid ShelterDTO animalShelter) {
+        AnimalShelter shelter = ShelterAdapter.fromDTO(animalShelter);
+
+        AnimalShelter savedShelter = animalShelterRepository.save(shelter);
+
+        return ShelterAdapter.toDTO(savedShelter);
+
     }
 
-    public AnimalShelter updateShelter(Integer id, AnimalShelter animalShelter) {
-        Optional<AnimalShelter> oldShelter = animalShelterRepository.findById(id);
-        if(oldShelter.isPresent()){
-            animalShelter.setId(id);
-            return animalShelterRepository.save(animalShelter);
+    @Validated(OnUpdate.class)
+    public ShelterDTO updateShelter(Integer id,@Valid ShelterDTO animalShelter) {
+        AnimalShelter shelter = getShelterById(id);
+        if(!shelter.getId().equals(animalShelter.getId())) {
+
+            throw new RuntimeException("Id of entity not the same with path id");
         }
-
-        throw new EntityNotFoundException("Shelter with id " + id + " not found");
+        return ShelterAdapter
+                .toDTO(animalShelterRepository
+                        .save(ShelterAdapter.fromDTO(animalShelter)));
     }
 
-    public AnimalShelter findById(Integer id) {
-        Optional<AnimalShelter> optional = animalShelterRepository.findById(id);
+    public ShelterDTO findById(Integer id) {
 
-        return optional.orElseThrow( () -> new EntityNotFoundException("Shelter with id " + id + " not found"));
+        AnimalShelter shelter = getShelterById(id);
+        return ShelterAdapter.toDTO(shelter);
+
 
     }
 
@@ -68,5 +102,94 @@ public class AnimalShelterService
         throw new EntityNotFoundException("Shelter with id " + shelterId + " not found");
     }
 
-    //continuam request-urile pentru pisici si catei
+
+
+
+    private AnimalShelter getShelterById(Integer id) {
+        Optional<AnimalShelter> optional = animalShelterRepository.findById(id);
+        return optional.orElseThrow(() -> new EntityNotFoundException("Shelter with id " + id + " not found"));
+    }
+
+    public Cat updateCatInShelter(Integer shelterId, Integer catId, Cat cat) {
+        AnimalShelter shelter = getShelterById(shelterId);
+
+        List<Cat> newCats = shelter.getCats().stream().map(c -> {
+
+            if(c.getId().equals(catId)){
+
+                cat.setId(catId);
+
+                return cat;
+            }
+            return c;
+        }).collect(Collectors.toList());
+
+        shelter.setCats(newCats);
+
+        animalShelterRepository.save(shelter);
+
+        return cat;
+    }
+
+    public void deleteCatInShelter(Integer shelterId, Integer catId) {
+        AnimalShelter shelter = getShelterById(shelterId);
+        List<Cat> newCats = shelter.getCats().stream().filter(c -> !c.getId().equals(catId)).collect(Collectors.toList());
+
+        shelter.setCats(newCats);
+        animalShelterRepository.save(shelter);
+    }
+
+    public List<Dog> findAllDogsByShelter(Integer shelterId) {
+        AnimalShelter shelter = getShelterById(shelterId);
+        return shelter.getDogs();
+    }
+
+    public List<Dog> addNewDogToShelter(Integer shelterId, Dog dog) {
+        AnimalShelter shelter = getShelterById(shelterId);
+
+        shelter.getDogs().add(dog);
+
+        animalShelterRepository.save(shelter);
+
+        return shelter.getDogs();
+    }
+
+    public Dog updateDogInShelter(Integer shelterId, Integer dogId, Dog dog) {
+        AnimalShelter shelter =getShelterById(shelterId);
+
+        List<Dog> newDogs = shelter.getDogs().stream().map(d ->{
+
+            if(d.getId().equals(dogId)){
+
+                dog.setId(dogId);
+
+                return dog;
+            }
+            return d;
+
+        }).collect(Collectors.toList());
+
+        shelter.setDogs(newDogs);
+
+        animalShelterRepository.save(shelter);
+
+        return dog;
+    }
+
+    public void deleteDogInShelter(Integer shelterId, Integer dogId) {
+        AnimalShelter shelter = getShelterById(shelterId);
+
+        boolean removed = shelter.getDogs().removeIf(d -> d.getId().equals(dogId));
+
+        animalShelterRepository.save(shelter);
+
+        if(!removed){
+            throw new RuntimeException("Already deleter or entity missing");
+        }
+    }
+
+
+
+    //DTO = data transfer object
+    //Controller-ul e dependent de service. Service-ul e dependent de Repository.
 }
